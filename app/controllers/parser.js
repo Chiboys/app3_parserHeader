2b9c0c0acae0467bc6842be9995f76ba3a6614f2
chiboy <chiboygm1@gmail.com>
2016-08-01 17:34:46 +0800

    add the json data

--- app/controllers/parser.js
+++ app/controllers/parser.js
@@ -1,6 +1,13 @@
 "use strict";
 module.exports = function(req,res){
 	var header = req.headers;
+	var language = header.accept-language.repalce(/(^[a-z]+-[a-z]+)/gi,"$1");
+	var sys = header.user-agent.exec(/^\s\(.*\)/)[0];
+	var json = {
+		"ip"£ºheader.x-forwarded-for;
+		"language":language;
+		"operating System":sys;
+	};
 	//console.log();
 	res.send(header);
 }
